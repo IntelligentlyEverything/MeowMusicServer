887d2f96033ac8edcf9cb9c03ebf6b853f462a1c
moecinnamo <abcd2890000456@gmail.com>
2025-08-28 14:42:43 +0800

    moveing indexHandler

--- main.go
+++ main.go
@@ -33,13 +33,3 @@ func main() {
 		fmt.Printf("%s Failed to start server: %v\n", TAG, err)
 	}
 }
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Server", "MeowMusicServer")
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	if r.URL.Path != "/" {
-		NotFoundHandler(w, r)
-		return
-	}
-	fmt.Fprintf(w, "<h1>音乐服务器</h1>")
-}
